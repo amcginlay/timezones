68306f48beb3455240930a13bdb34d20f4f41e05
Alan McGinlay <amcginlay@Alans-MPB.lan>
2017-06-24 13:54:54 +0100

    initial

---
+++ src/main/java/com/cynaptec/timezones/TimezonesApplication.java
@@ -0,0 +1,12 @@
+package com.cynaptec.timezones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimezonesApplication.class, args);
+	}
+}
