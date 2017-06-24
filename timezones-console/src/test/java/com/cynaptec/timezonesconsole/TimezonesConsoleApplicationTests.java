249b3fca8daa9659692da0c2ac5d289fa038b024
Alan McGinlay <amcginlay@Alans-MPB.lan>
2017-06-24 15:12:46 +0100

    introducing timezones-console and renaming some core identifiers (2nd attempt)

---
+++ timezones-console/src/test/java/com/cynaptec/timezonesconsole/TimezonesConsoleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cynaptec.timezonesconsole;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimezonesConsoleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ timezones-core/src/main/java/com/cynaptec/timezonescore/TimezonesCoreApplication.java
@@ -0,0 +1,12 @@
+package com.cynaptec.timezonescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezonesCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimezonesCoreApplication.class, args);
+	}
+}
